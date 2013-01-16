4ad9a59a44cf339f0e2bc14f836d8d0df4ea9f43
Owen Gunden <ogunden@phauna.org>
2013-01-16 14:48:27 -0500

    add gitignore file and tweak installation

--- install-release.sh
+++ install-release.sh
@@ -8,4 +8,4 @@ fi
 reldir="/home/arguments/releases/$(date +%s)"
 mkdir -p ${reldir}
 ln -sfT ${reldir} /home/arguments/releases/latest
-cp -r arguments-release.js ${reldir}/.
+cp -r arguments.js ${reldir}/.
