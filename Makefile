abd7b726315a968a9ea9bcebe676b9347d9f6917
Owen Gunden <ogunden@phauna.org>
2013-01-16 14:44:10 -0500

    release installing

--- Makefile
+++ Makefile
@@ -7,5 +7,9 @@ all: $(NAME)
 $(NAME): $(SRC)
 	opa $(SRC) -o $(NAME)
 
+install-release: $(NAME)
+	./install-release.sh
+
 clean:
 	rm -f error.log access.log package.json $(NAME)
+
---
+++ install-release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ $(whoami) != 'arguments' ]]; then
+  echo "this must be run by arguments user"
+  exit 2
+fi
+
+reldir="/home/arguments/releases/$(date +%s)"
+mkdir -p ${reldir}
+ln -sfT ${reldir} /home/arguments/releases/latest
+cp -r arguments-release.js ${reldir}/.
